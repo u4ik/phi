e7740f6c832fe8ee7f0df31b874dff1bd4f9b093
u4ik <affirmativeamit@gmail.com>
2020-09-10 00:37:59 -0400

    What it's left on??

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import logo from './assets/megamillogo.svg'
 
 import './App.css';
 import Generator from './components/Generator'
+import Canvas from './components/Canvas'
 
 
 
@@ -14,7 +15,8 @@ function App() {
 
   const appStyle = {
     // backgroundImage: `url(${backgroundImg})`,
-    background: 'linear-gradient(0deg, royalblue, blue, darkblue ,navy, cyan 99%,lightblue )',
+    // background: 'linear-gradient(0deg, royalblue, blue, darkblue ,navy, cyan 99%,lightblue )',
+    background: 'linear-gradient(0deg, black, white, lightgrey ,grey, darkgrey 99%,black )',
     minHeight: '100vh',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
@@ -25,9 +27,10 @@ function App() {
     <div className="App" style={appStyle}>
       <div style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
         <div style={{display:'flex', flexDirection:'row', justifyContent:'center', marginTop:'2%',  filter:'drop-shadow(10px 10px 10px black)'}}>
-          <img src={logo} style={{ width: '30%', filter:'drop-shadow(4px 4px 2px white)',     backgroundColor: 'rgba(0, 45, 125, .1)',paddingLeft:'30%',paddingRight:'30%', paddingTop: '1%', paddingBottom: '1%' ,borderRadius: '3%'}} />
+          {/* <img src={logo} style={{ width: '30%', filter:'drop-shadow(4px 4px 2px white)',     backgroundColor: 'rgba(0, 45, 125, .1)',paddingLeft:'30%',paddingRight:'30%', paddingTop: '1%', paddingBottom: '1%' ,borderRadius: '3%'}} /> */}
         </div>
-        <Generator />
+        {/* <Generator /> */}
+        <Canvas/>
       </div>
     </div>
   );
---
+++ src/components/Canvas.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Canvas = () => {
+
+    useEffect(() => {
+
+
+        let canvas = document.getElementById('canvas1');
+        const ctx = canvas.getContext('2d');
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        ctx.globalCompositeOperation = 'destination-over'
+
+        // let size = 0;
+
+        let number = 0;
+        // let scale = Math.random() * 10 / Math.PI * 1 ;
+        let scale = 3
+        let lineW = .008
+
+        function drawFlower() {
+            let angle = number * 1
+            let radius = scale * Math.sqrt(number + angle);
+            let x = radius * Math.sin(angle) + canvas.width / 2;
+            let y = radius * Math.cos(angle) + canvas.height / 2;
+            ctx.fillStyle = 'black';
+            // ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
+            ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
+            // ctx.strokeStyle = 'black'
+            ctx.lineWidth = lineW
+                // ctx.lineWidth = 5
+            ctx.beginPath();
+            ctx.arc(x, y, 80, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fill();
+            ctx.stroke();
+            // number+  Math.random() * 100 + 5
+            number++
+            lineW += .01
+            scale += .01
+                // lineW = 3 + Math.random() * 10
+
+
+
+
+        }
+
+        function animate() {
+            // ctx.clearRect(0,0,canvas.width, canvas.height)
+            drawFlower();
+            // if(number > 300) return ;
+            requestAnimationFrame(animate);
+
+        }
+        animate();
+    });
+
+    return ( <
+        div >
+
+        <
+        canvas id = 'canvas1'
+        style = {
+            { position: 'absolute', top: '0', left: '0' } } >
+
+
+        <
+        /canvas> <
+        /div>
+    )
+
+}
+
+export default Canvas;
