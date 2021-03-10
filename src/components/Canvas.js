e71083c93a04fdeaa88f93a55945c78b568c499f
u4ik <affirmativeamit@gmail.com>
2021-03-10 09:48:37 -0500

    update

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -19,8 +19,8 @@ const Canvas = () => {
             let x = radius * Math.sin(angle) + canvas.width / 2;
             let y = radius * Math.cos(angle) + canvas.height / 2;
             ctx.fillStyle = number % 2 == 0 ? `rgba(255,255,255,${opacity2})` : `rgba(0,0,0,${opacity2})`;
-            ctx.strokeStyle = number % 2 == 0 ? `rgba(0,0,0,${opacity})` : `rgba(255,255,255,${opacity})`
-            ctx.lineWidth = lineW
+            ctx.strokeStyle = number % 2 == 0 ? `rgba(0,0,0,${opacity})` : `rgba(255,255,255,${opacity})`;
+            ctx.lineWidth = lineW;
             ctx.beginPath();
             ctx.arc(x, y, Math.PI, 0, Math.PI * 3);
             ctx.closePath();
