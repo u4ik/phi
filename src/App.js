522ca1e5cfcfbf3cd27c0f78a67d3e8510cf3b1c
u4ik <affirmativeamit@gmail.com>
2021-03-10 09:42:04 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-
-// import backgroundImg from './assets/background.jpg'
-import backgroundImg from './assets/background1.jpeg'
-
-import logo from './assets/megamillogo.svg'
-
 import './App.css';
-import Generator from './components/Generator'
+
 import Canvas from './components/Canvas'
 
 
@@ -14,8 +8,7 @@ import Canvas from './components/Canvas'
 function App() {
 
   const appStyle = {
-    // backgroundImage: `url(${backgroundImg})`,
-    // background: 'linear-gradient(0deg, royalblue, blue, darkblue ,navy, cyan 99%,lightblue )',
+ 
     background: 'linear-gradient(0deg, black, white, lightgrey ,grey, darkgrey 99%,black )',
     minHeight: '100vh',
     backgroundSize: 'cover',
@@ -27,7 +20,7 @@ function App() {
     <div className="App" style={appStyle}>
       <div style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
         <div style={{display:'flex', flexDirection:'row', justifyContent:'center', marginTop:'2%',  filter:'drop-shadow(10px 10px 10px black)'}}>
-          {/* <img src={logo} style={{ width: '30%', filter:'drop-shadow(4px 4px 2px white)',     backgroundColor: 'rgba(0, 45, 125, .1)',paddingLeft:'30%',paddingRight:'30%', paddingTop: '1%', paddingBottom: '1%' ,borderRadius: '3%'}} /> */}
+ 
         </div>
         {/* <Generator /> */}
         <Canvas/>
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,75 +1,53 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useEffect } from 'react';
 
 const Canvas = () => {
-
     useEffect(() => {
-
-
         let canvas = document.getElementById('canvas1');
         const ctx = canvas.getContext('2d');
-
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-
-        ctx.globalCompositeOperation = 'destination-over'
-
+        ctx.globalCompositeOperation = 'destination-over';
         // let size = 0;
-
         let number = 0;
-        // let scale = Math.random() * 10 / Math.PI * 1 ;
-        let scale = 3
-        let lineW = .008
-
-        function drawFlower() {
-            let angle = number * 1
+        let scale = .9;
+        let lineW = .3;
+        let opacity = 0;
+        let opacity2 = 0;
+        function drawPhi() {
+            let angle = number;
             let radius = scale * Math.sqrt(number + angle);
             let x = radius * Math.sin(angle) + canvas.width / 2;
             let y = radius * Math.cos(angle) + canvas.height / 2;
-            ctx.fillStyle = 'black';
-            // ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
-            ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
-            // ctx.strokeStyle = 'black'
+            ctx.fillStyle = number % 2 == 0 ? `rgba(255,255,255,${opacity2})` : `rgba(0,0,0,${opacity2})`;
+            ctx.strokeStyle = number % 2 == 0 ? `rgba(0,0,0,${opacity})` : `rgba(255,255,255,${opacity})`
             ctx.lineWidth = lineW
-            // ctx.lineWidth = 5
             ctx.beginPath();
-            ctx.arc(x, y, 80, 0, Math.PI * 2);
+            ctx.arc(x, y, Math.PI, 0, Math.PI * 3);
             ctx.closePath();
             ctx.fill();
             ctx.stroke();
-            // number+  Math.random() * 100 + 5
             number++
-            lineW += .01
-            scale += .01
-            // lineW = 3 + Math.random() * 10
-
-
-
-
-        }
-
+            lineW += .1
+            scale += .001
+            opacity += .0001
+            opacity2 += .001
+        };
         function animate() {
-            // ctx.clearRect(0,0,canvas.width, canvas.height)
-            drawFlower();
-            // if(number > 300) return ;
+            drawPhi();
             requestAnimationFrame(animate);
-
-        }
+        };
         animate();
-    });
+    }, []);
 
     return (
-        <div >
-
+        <div>
             <canvas id='canvas1'
                 style={
                     { position: 'absolute', top: '0', left: '0' }
                 } >
-
-
-            </canvas> </div>
+            </canvas>
+        </div>
     )
-
-}
+};
 
 export default Canvas;
